3bc037929bfe2ad883b0b49f9c45a64407e8850c
eloygarza10 <eloy.garza@diginteligencia.com>
2017-04-27 15:28:07 -0500

    Update diagonales.js

--- diagonales.js
+++ diagonales.js
@@ -1,10 +1,15 @@
-var x = 10;
-
-for (i=1; i<x; i++){
-  var str1 = '';
-  for (j=1; j<x; j++){
-    if (i==j){str1 += '0'}
-    else {str1 += '*'}
+function Ceros(veces){
+/*var veces = 10;*/
+if(veces <= 0) {console.log("Numero Incorrecto") }
+else {
+for (i=1; i<veces; i++){
+  var stars = '';
+  for (j=1; j<veces; j++){
+    if (i==j){stars += '0'}
+    else {stars += '*'}
   }
-  console.log(str1)
+  console.log(stars)
 }
+}
+}
+Ceros(-10)
