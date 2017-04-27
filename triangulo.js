c3b872a588e89c806ae2a27327f16c57b83c0e14
eloygarza10 <eloy.garza@diginteligencia.com>
2017-04-27 14:36:55 -0500

    Agrego Archivos de Triangulo

---
+++ triangulo.js
@@ -0,0 +1,18 @@
+var stars = '*';
+var veces = 10;
+
+  for (i=1; i<veces; i++){
+    console.log(stars);
+    stars += '*';
+  }
+
+  /* reversa*/
+
+var veces = 5;
+for (j=veces; j>1; j--){
+  var stars = '*';
+  for (i=1; i<j; i++){
+    stars += '*';
+  }
+  console.log(str1)
+}
