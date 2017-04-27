57499b59390218a7456999decb2bedd14aa1c1bf
eloygarza10 <eloy.garza@diginteligencia.com>
2017-04-27 15:27:37 -0500

    Update triangulo.js

--- triangulo.js
+++ triangulo.js
@@ -1,16 +1,20 @@
 function Triangle(veces) {
 var stars = '*';
 /*var veces = 10;*/
-
+if(veces <= 0) {console.log("Numero Incorrecto") }
+else {
   for (i=1; i<veces; i++){
     console.log(stars);
     stars += '*';
   }
+     }
   }
-Triangle (10)
+Triangle (-10)
 
   /*reversa*/
-function Reversa(){
+function Reversa(veces){
+if (veces <= 0) {console.log("NumeroIncorrectoR")}
+else {
 /*var veces = 5;*/
 for (j=veces; j>1; j--){
   var stars = '*';
@@ -20,4 +24,5 @@ for (j=veces; j>1; j--){
   console.log(stars)
 }
 }
-Reversa(5)
+}
+Reversa(-10)
