a86ba2aec4b783ca5ba0f5a2ddd8d3e935e78861
eloygarza10 <eloy.garza@diginteligencia.com>
2017-06-15 00:40:36 -0500

    CURP
    CURP

---
+++ Tarea6/fz.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+var names = document.getElementById('names')
+var FirstAp = document.getElementById('FirstAp')
+var SecondAp = document.getElementById('SecondAp')
+var h = document.getElementById('h')
+var m = document.getElementById('m')
+var day = document.getElementById('day')
+var month = document.getElementById('month')
+var year = document.getElementById('year')
+var federativeState = document.getElementById('federativeState')
+name.addEventListener('keyup', copy);
+var copy = document.getElementById('curp');
+
+}
